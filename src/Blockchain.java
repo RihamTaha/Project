9705c4d49d81709fdaeffe9f1ff0c7726fa14f25
Riham Taha <rehamtaha35@gmail.com>
2018-04-24 22:38:10 +0200

    Second Commit_Riham

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -8,16 +8,24 @@ public class Blockchain
     {
         Block Previous_Block;
         Block Current_Block;
+
+        Boolean Prev=false;
+        Boolean Curr=false;
         Boolean D=false;
         for(int k=1; k<blockChain.size();k++)
         {
+           D=false;
             Current_Block=blockChain.get(k);
             Previous_Block=blockChain.get(k-1);
             if(Current_Block.hash==blockChain.get(k).hash)
             {
-                D = true;
+                Curr=true;
             }
             if(Previous_Block.hash==blockChain.get(k).PrevHash)
+            {
+                Prev=true;
+            }
+            if(Prev&& Curr)
             {
                 D=true;
             }
